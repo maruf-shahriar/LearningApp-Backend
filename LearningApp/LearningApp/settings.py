f3dbc3a1ea193c9d1683ea1745612a90ea08cee5
Kasif Hasnaen Zisan <zisan321@gmail.com>
2023-07-22 19:45:19 +0600

    app password and database updated

--- LearningApp/LearningApp/settings.py
+++ LearningApp/LearningApp/settings.py
@@ -164,5 +164,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'webapp1714@gmail.com'
-EMAIL_HOST_PASSWORD = 'gyodzidaiqfffirv'
+EMAIL_HOST_PASSWORD = 'kywukuoyzrjtfobo'
 EMAIL_PORT = 587
